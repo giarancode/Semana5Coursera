3a24e22f6d04d45c029a4e89bac984b98a5504b6
Giarancode <giovannyarandia@gmail.com>
2020-10-26 21:38:28 -0600

    version 1

---
+++ app/src/main/java/com/example/semana3/fragment/InterfaceFragmentReciclerViewFragment.java
@@ -0,0 +1,16 @@
+package com.example.semana3.fragment;
+
+import com.example.semana3.Pojo.InformacionAtributos;
+import com.example.semana3.adaptador.AdaptadorCardView;
+
+import java.util.ArrayList;
+
+public interface InterfaceImplementsMethodsFragment {
+    public void generarVista();
+
+    public AdaptadorCardView crearAdaptador(ArrayList<InformacionAtributos> datos);
+
+    public void inicializaAdaptador(AdaptadorCardView adaptador);
+
+
+}
